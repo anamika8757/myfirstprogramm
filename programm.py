c819a9bc096db6ca2906352e8199dfc315edc8a9
anamika8757 <sanjeevsah8757814112@gmail.com>
2025-10-25 22:53:48 +0530

    added progarmm.py

---
+++ programm.py
@@ -0,0 +1,10 @@
+number=int(input("enter an integer"))
+if number % 2 == 0:
+    print("the number is even.")
+else:
+    print("the number is odd.")
+sum_total=0
+for i in range(1,51):
+    sum_total +=1
+    print("the sum of integer from 1 to 5 is:",sum_total)
+            
